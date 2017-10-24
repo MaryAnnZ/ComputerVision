19e6c45bbc78d15f42dbeb4ba52550ea4fbc2f22
bettina <betti.schlager@aon.at>
2017-10-24 17:15:21 +0200

    pseudocode ue1

--- UE1/Aufgabe 1/main.m
+++ UE1/Aufgabe 1/main.m
@@ -1,7 +1,27 @@
 % TODO
+redTmp =  imread('input/01112v_R.jpg');
 
-% 
+original = uint8(zeros(size(redTmp,1), size(redTmp,2), 3));
+original(:,:,1) = redTmp;
+original(:,:,2) = imread('input/01112v_G.jpg');
+original(:,:,3) = imread('input/01112v_B.jpg');
 
+imshow(original);
+% zuerst circshift 
 
-%original = repmat(imread('input/01112v_R.jpg')
-%imshow(test)s
+% resultimage = 0
+% higestREsult = 0
+% crop RED
+% schleife: -15 till 15 (horizontal and veritcal)
+% 1) cumpute circshiftIMG
+% 2) crop BLUE
+% 3) value = NCC (CropRed, cropBlue)
+% 4) if value > highestREsult;
+%    ---> higestReslut = value
+%    ---> resultImage = cirshiftIMG
+%
+
+% same R and G
+
+% merge resulting layers
+
