c658cced6f39748703a5296d082bd0ce3bae07c1
bettina <betti.schlager@aon.at>
2017-10-24 17:39:23 +0200

    beginning UE2

--- UE1/Aufgabe 2/main.m
+++ UE1/Aufgabe 2/main.m
@@ -1,1 +1,13 @@
 %TODO
+%
+%
+% D = x
+%
+% 1. Choose random starting values for the centroids c. (k-mal)
+% 2. Assign all data points to their nearest cluster centroids
+% 3. Compute the new cluster centroids as the mean of all data points 
+%   assigned to that cluster:
+% 4. Compute J and check for convergence. For this purpose, compute the ratio 
+%   between the old and the new J. If the ratio lies under a given threshold, 
+%   terminate the clustering, otherwise go to point 2.
+%
