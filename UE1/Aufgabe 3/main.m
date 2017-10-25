1365cb04b1acb676565afbdce3c0d56e1afba71d
Daniel Heger <e1026721@student.tuwien.ac.at>
2017-10-25 16:52:33 +0200

    implemented filtering, output = 10 scale-filtered images

--- UE1/Aufgabe 3/main.m
+++ UE1/Aufgabe 3/main.m
@@ -19,17 +19,32 @@ sigma = 2;
 k = 1.25; %aka scale
 levels = 10;
 I1 = imread('input/butterfly.jpg');
-I2 = imread('input/ownimg.jpg');
+I2 = rgb2gray(imread('input/ownimg.jpg'));
 
 scalespace1 = zeros(size(I1,1), size(I1,2), levels);
 scalespace2 = zeros(size(I2,1), size(I2,2), levels);
 
-for i = 1:10
-    i
+for i = 1:levels
+    % create filter for current pass
+    currentFilterSize = 2*floor(3*sigma)+1;
+    currentFilter = fspecial('log', currentFilterSize, sigma);
+    
+    % scale-normalisation of the filter (i.e. on values in filter)
+    if i ~= 1
+        currentFilter = currentFilter .* sigma^2;
+    end
+    
+    % convolution
+    scalespace1(:,:,i) = imfilter(I1, currentFilter, 'same', 'replicate');
+    %scalespace2(:,:,i) = imfilter(I2, currentFilter, 'same', 'replicate');
+    
+    % update sigma
+    sigma = sigma * k;
+    
+    figure, imshow(scalespace1(:,:,i));
 end
 
 
-
-% todo Frage: scalespace interpolieren zwischen den Scales): index mal
+% TODO Frage: scalespace interpolieren zwischen den Scales): index mal
 % scale + aufrunden
 
