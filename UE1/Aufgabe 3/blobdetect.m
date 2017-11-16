deb89b9f7fc1861f4760549e958d49477cfda71b
Daniel Heger <e1026721@student.tuwien.ac.at>
2017-11-16 20:32:58 +0100

    added final ownimg.jpg now with a function for blobdetection instead of doing everything twice TODO: report-stuff

--- UE1/Aufgabe 3/blobdetect.m
+++ UE1/Aufgabe 3/blobdetect.m
@@ -1,7 +1,57 @@
-function [ output_args ] = blobdetect( input_args )
-%UNTITLED Summary of this function goes here
+function [ ] = blobdetect(imgpath, sigma, scaleFactor, levels, thres )
+%BLOBDETECT Summary of this function goes here
 %   Detailed explanation goes here
 
+I = imread(imgpath);
+if size(I,3) == 3
+    I = rgb2gray(I);
+end
+I = im2double(I);
+
+scalespace = zeros(size(I,1), size(I,2), levels);
+
+allSigmas = []; % saving all sigma sizes for later use in show_all_circles
+
+for i = 1:levels
+    % create filter for current pass
+    currentFilterSize = 2*floor(3*sigma)+1;
+    currentFilter = fspecial('log', currentFilterSize, sigma);
+    
+    % scale-normalisation of the filter (i.e. on values in filter)
+    if i ~= 1
+        currentFilter = currentFilter .* sigma^2;
+    end
+    
+    % convolution
+    scalespace(:,:,i) = imfilter(I, currentFilter, 'same', 'replicate');
+    
+    % save and update sigma
+    allSigmas = cat(1, allSigmas, sigma);
+    sigma = sigma * scaleFactor;
+    
+    %figure, imshow(scalespace(:,:,i)); %DEBUG
+end
+
+% convert to absolute values
+scalespace = abs(scalespace);
+
+% apply threshold
+thresFlag = scalespace > thres;
+scalespace = thresFlag .* scalespace;
+
+% maxima detection
+max = imregionalmax(scalespace);
+
+circleRows = []; circleCols = []; circleRads = [];
+
+for i=1:levels
+    [row, col] = find(max(:,:,i)); % find all ones in logical img
+    circleRows = cat(1, circleRows, row); % add maxima rows to row coordinate vector
+    circleCols = cat(1, circleCols, col); % add maxima cols to col coordinate vector
+    circleRads = cat(1, circleRads, repmat(allSigmas(i) * 2^(1/2), length(col), 1));
+end
+
+figure, show_all_circles(I, circleCols, circleRows, circleRads);
 
 end
 
