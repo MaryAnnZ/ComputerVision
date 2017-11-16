8c738e64ed11401bd1a72a7bdb463f8d3dbe9d5a
Daniel Heger <e1026721@student.tuwien.ac.at>
2017-11-16 19:47:23 +0100

    2nd part of Aufgabe 3 done - already drawing nice circles :) TODO: code cleanup

--- UE1/Aufgabe 3/main.m
+++ UE1/Aufgabe 3/main.m
@@ -1,30 +1,17 @@
-% TODO
-% % 1a
-% erstelle scale space (levels tief)
-% erstelle LoG filter (fspecial) mit currentSigma
-% 
-% % 1b
-% Schleife (über #levels in scale space) 
-%     conv image with filter
-%     save LoG response
-%     update LoG filter
-%     scale picture
-% end
-% 
-% %2
-% suche in der 8er Neighborhood + oben + unten
-
 close all
 
+I1 = im2double(imread('input/butterfly.jpg'));
+I2 = im2double(rgb2gray(imread('input/ownimg.jpg')));
+thres = 0.15;
 % values recommended by Angabe
 sigma = 2;
 k = 1.25; %aka scale
 levels = 10;
-I1 = im2double(imread('input/butterfly.jpg'));
-I2 = im2double(rgb2gray(imread('input/ownimg.jpg')));
 
 scalespace1 = zeros(size(I1,1), size(I1,2), levels);
 scalespace2 = zeros(size(I2,1), size(I2,2), levels);
+
+allSigmas = []; % saving all sigma sizes for later use in show_all_circles
 
 for i = 1:levels
     % create filter for current pass
@@ -40,14 +27,42 @@ for i = 1:levels
     scalespace1(:,:,i) = imfilter(I1, currentFilter, 'same', 'replicate');
     scalespace2(:,:,i) = imfilter(I2, currentFilter, 'same', 'replicate');
     
-    % update sigma
+    % save and update sigma
+    allSigmas = cat(1, allSigmas, sigma);
     sigma = sigma * k;
     
-    figure, imshow(scalespace1(:,:,i));
+    %figure, imshow(scalespace1(:,:,i));
     %figure, imshow(scalespace2(:,:,i));
 end
 
+% convert to absolute values
+scalespace1 = abs(scalespace1);
+scalespace2 = abs(scalespace2);
 
-% TODO Frage: scalespace interpolieren zwischen den Scales): index mal
-% scale + aufrunden
+% apply threshold
+thresFlag1 = scalespace1 > thres;
+thresFlag2 = scalespace2 > thres;
+scalespace1 = thresFlag1 .* scalespace1;
+scalespace2 = thresFlag2 .* scalespace2;
 
+% maxima detection
+max1 = imregionalmax(scalespace1);
+max2 = imregionalmax(scalespace2);
+
+circleRows1 = []; circleCols1 = []; circleRads1 = [];
+circleRows2 = []; circleCols2 = []; circleRads2 = [];
+
+for i=1:levels
+    [row1, col1] = find(max1(:,:,i)); % find all ones in logical img
+    circleRows1 = cat(1, circleRows1, row1); % add maxima rows to row coordinate vector
+    circleCols1 = cat(1, circleCols1, col1); % add maxima cols to col coordinate vector
+    circleRads1 = cat(1, circleRads1, repmat(allSigmas(i) * 2^(1/2), length(col1), 1));
+
+    [row2, col2] = find(max2(:,:,i)); % find all ones in logical img
+    circleRows2 = cat(1, circleRows2, row2); % add maxima rows to row coordinate vector
+    circleCols2 = cat(1, circleCols2, col2); % add maxima cols to col coordinate vector
+    circleRads2 = cat(1, circleRads2, repmat(allSigmas(i) * 2^(1/2), length(col2), 1));
+end
+
+show_all_circles(I1, circleCols1, circleRows1, circleRads1);
+%show_all_circles(I2, circleCols2, circleRows2, circleRads2);
