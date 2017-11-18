9407e0ff38c432d984b692b65f53b0a86d50b36a
Daniel Heger <e1026721@student.tuwien.ac.at>
2017-11-18 14:57:35 +0100

    added code for report stuff added output images

--- UE1/Aufgabe 3/main.m
+++ UE1/Aufgabe 3/main.m
@@ -7,5 +7,7 @@ k = 1.25; %aka scale
 levels = 10;
 
 blobdetect('input/butterfly.jpg', sigma, k, levels, thres);
+blobdetect('input/butterfly_halfsize.jpg', sigma, k, levels, thres);
+
 blobdetect('input/ownimg.jpg', sigma, k, levels, thres);
-blobdetect('input/ownimg_tmp.jpg', sigma, k, levels, thres);
+blobdetect('input/ownimg_halfsize.jpg', sigma, k, levels, thres);
